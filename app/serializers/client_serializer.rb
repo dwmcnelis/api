7680da8572bf23915777b1b69ea15346dac024c6
Dave McNelis <davemcnelis@market76.com>
2015-03-20 09:32:28 -0400

    Add image urls to clients

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,3 +1,8 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :level, :rank, :status, :buzzes, :notes
+  attributes :id, :first_name, :last_name, :email, :phone, :level, :rank, :status, :buzzes, :notes, :image
+
+  def image
+    'http://localhost:3002'+object.image.url
+  end
+
 end
